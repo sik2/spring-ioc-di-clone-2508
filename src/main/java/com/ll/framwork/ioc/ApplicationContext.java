786005f93e3090b84674a0338795fb52a58dd24d
sik2 <bickwang123@gmail.com>
2025-08-19 14:49:02 +0900

    testPostService singletone, g

--- src/main/java/com/ll/framwork/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framwork/ioc/ApplicationContext.java
@@ -2,11 +2,16 @@ package com.ll.framwork.ioc;
 
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
+    private Map<String, Object> beans;
     private String basePackage;
 
     public ApplicationContext(String basePackage) {
         this.basePackage = basePackage;
+        beans = new HashMap<>();
     }
 
     public void init() {
@@ -14,10 +19,15 @@ public class ApplicationContext {
     }
 
     public <T> T genBean(String beanName) {
-        if (beanName.equals("testPostService")) {
-            return (T) new TestPostService();
-        }
+        Object bean = beans.get(beanName);
 
-        return null;
+        if (bean == null) {
+            bean = switch (beanName) {
+                case "testPostService" -> new TestPostService();
+                default ->  null;
+            };
+            beans.put(beanName, bean);
+        }
+        return (T) bean;
     }
 }
