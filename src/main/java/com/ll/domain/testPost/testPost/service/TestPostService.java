c29d14a15c0c6837e41d94a5a9123bc5362b30d0
sik2 <bickwang123@gmail.com>
2025-08-19 15:17:23 +0900

    testPostService has testPostRepository, r

--- src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
+++ src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
@@ -1,7 +1,11 @@
 package com.ll.domain.testPost.testPost.service;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.framwork.annotations.Service;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Service
 public class TestPostService {
+    private final TestPostRepository testPostRepository;
 }
