d01749f88839ee974249f0de709a86a1ff6cde40
sik2 <bickwang123@gmail.com>
2025-08-21 16:29:52 +0900

    ClsUtil.construct b

--- src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
+++ src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
@@ -18,7 +18,19 @@ public class ClsUtilTest {
     @Test
     @DisplayName("ClsUtil.construct")
     void t2() {
+        // new TestCar("BMW", 1234); 와 같은 의미
         TestCar testCar = ClsUtil.construct("com.ll.framework.ioc.util.sample.TestCar", new Object[]{"BMW", 1234});
+
+        assertThat(testCar.getName()).isEqualTo("BMW");
+        assertThat(testCar.getNumber()).isEqualTo(1234);
+    }
+
+
+    @Test
+    @DisplayName("ClsUtil.construct with cls")
+    void t3() {
+        // new TestCar("BMW", 1234); 와 같은 의미
+        TestCar testCar = ClsUtil.construct(TestCar.class, new Object[]{"BMW", 1234});
 
         assertThat(testCar.getName()).isEqualTo("BMW");
         assertThat(testCar.getNumber()).isEqualTo(1234);
