7ffa5b256855c1ae1a0e374ef731830280ef50c2
sik2 <bickwang123@gmail.com>
2025-08-21 16:28:01 +0900

    ClsUtil.construct r

--- src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
+++ src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
@@ -14,4 +14,13 @@ public class ClsUtilTest {
 
         assertThat(cls).isEqualTo(TestCar.class);
     }
+
+    @Test
+    @DisplayName("ClsUtil.construct")
+    void t2() {
+        TestCar testCar = ClsUtil.construct("com.ll.framework.ioc.util.sample.TestCar", new Object[]{"BMW", 1234});
+
+        assertThat(testCar.getName()).isEqualTo("BMW");
+        assertThat(testCar.getNumber()).isEqualTo(1234);
+    }
 }
--- src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
+++ src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
@@ -1,4 +1,11 @@
 package com.ll.framework.ioc.util.sample;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TestCar {
+    String name;
+    int number;
 }
